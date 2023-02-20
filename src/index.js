6ff909eb59220ecccee37e855baf59cc789409a6
Rinslet-Volker <chenaia@mail.ru>
2023-02-20 22:17:05 +0300

    solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsConfigStr = bracketsConfig.map(el => el.join(''));
+
+  while (true) {
+    let lengthBefore = str.length;
+
+    bracketsConfigStr.forEach(el => {
+      str = str.replace(el, '');
+    })
+
+    if (str.length === 0) {
+      return true;
+    }
+
+    if (str.length === lengthBefore) {
+      return false;
+    }
+  }
+
 }
